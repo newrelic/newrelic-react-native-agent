43fc6a25d0f59cbc17f3f1b4446e4424bfea571d
Nisarg Desai <ndesai@newrelic.com>
2022-01-21 10:47:36 -0600

    refactor: update  App with examples of features

--- NewRelicExampleApp/App.js
+++ NewRelicExampleApp/App.js
@@ -1,38 +1,199 @@
-'use strict';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  AccessibilityInfo
-} from 'react-native';
-import SearchPage from './pages/SearchPage';
-import SearchResults from './pages/SearchResults';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
-const Stack = createNativeStackNavigator();
-
-export default class App extends Component<{}> {
-  render() {
-    return (
-
-<NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={SearchPage}
-          options={{ title: 'Welcome' }}
-        />
-        <Stack.Screen name="SearchResults" component={SearchResults} />
-      </Stack.Navigator>
-    </NavigationContainer>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
+ import React from 'react';
+ import NewRelic from 'newrelic-react-native-agent';
+ import type {Node} from 'react';
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   Button
+ } from 'react-native';
+ import {
+   Colors,
+   DebugInstructions,
+   Header,
+   LearnMoreLinks,
+   ReloadInstructions,
+ } from 'react-native/Libraries/NewAppScreen';
+ 
+ const Section = ({children, title}): Node => {
+   const isDarkMode = useColorScheme() === 'dark';
+   return (
+     <View style={styles.sectionContainer}>
+       <Text
+         style={[
+           styles.sectionTitle,
+           {
+             color: isDarkMode ? Colors.white : Colors.black,
+           },
+         ]}>
+         {title}
+       </Text>
+       <Text
+         style={[
+           styles.sectionDescription,
+           {
+             color: isDarkMode ? Colors.light : Colors.dark,
+           },
+         ]}>
+         {children}
+       </Text>
+     </View>
+   );
+ };
+ 
+ const App: () => Node = () => {
+   const isDarkMode = useColorScheme() === 'dark';
+ 
+   const backgroundStyle = {
+     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+   };
+ 
+   //Http Error and InterAction Example
+   const badApiLoad = async () => {
+     const interactionId = await NewRelic.startInteraction('StartLoadBadApiCall');
+     console.log(interactionId);
+     const url = 'https://facebook.github.io/react-native/moviessssssssss.json';
+     fetch(url)
+       .then((response) => response.json())
+       .then((responseJson) => {
+         console.log(responseJson);
+         NewRelic.endInteraction(interactionId);
+       }) .catch((error) => {
+         NewRelic.endInteraction(interactionId);
+         console.error(error);
+       });;
+   };
+ 
+   // Http Request and Interaction Example
+   const goodApiLoad = async () => {
+     const interactionId = await NewRelic.startInteraction('StartLoadGoodApiCall');
+     console.log(interactionId);
+     const url = 'https://facebook.github.io/react-native/movies.json';
+ 
+     NewRelic.recordBreadcrumb("Http Request Start",{
+       "url":url,
+       "starttine":new Date()
+     })
+     fetch(url)
+       .then((response) => response.json())
+       .then((responseJson) => {
+         NewRelic.recordBreadcrumb("Http Request Success",{
+           "response":responseJson,
+           "url":url,
+           "endtime":new Date()
+         })
+         console.log(responseJson);
+         NewRelic.endInteraction(interactionId);
+       })
+       .catch((error) => {
+         NewRelic.endInteraction(interactionId);
+         console.error(error);
+       });
+   };
+ 
+   //Promise Rejection Example
+   // if user is not handling error in catch then it captures by our prmose rejection handler
+   const promiseRejection = async () => {
+     const interactionId = await NewRelic.startInteraction('PromiseRejectionCall');
+     console.log(interactionId);
+     const url = 'https://facebook.github.io/react-native/moviessssssssss.json';
+     fetch(url)
+       .then((response) => response.json())
+       .then((responseJson) => {
+         console.log(responseJson);
+         NewRelic.endInteraction(interactionId);
+       }) 
+   };
+ 
+   // JS Error Example
+   // When Javascript throws error it captures by our Error Handler
+   const throwError = () => {
+      
+      throw new Error("I am Error");
+   }
+ 
+   return (
+     <SafeAreaView style={backgroundStyle}>
+       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+       <Button title={'Bad API'} onPress={badApiLoad} color={'#3365f3'} />
+ 
+       <View style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,height: 10
+           }}></View>
+ 
+       <Button title={'Good API'} onPress={goodApiLoad} color={'#3365f3'} />
+ 
+       <View style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,height: 10
+           }}></View>
+ 
+      <Button title={'Promise Rejection Example'} onPress={promiseRejection} color={'#3365f3'} />
+ 
+      <View style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,height: 10
+           }}></View>
+ 
+      <Button title={'Error Example'} onPress={throwError} color={'#3365f3'} />
+ 
+ 
+       <ScrollView
+         contentInsetAdjustmentBehavior="automatic"
+         style={backgroundStyle}>
+         <Header />
+         <View
+           style={{
+             backgroundColor: isDarkMode ? Colors.black : Colors.white,
+           }}>
+           <Section title="Step One">
+             Edit <Text style={styles.highlight}>App.js</Text> to change this
+             screen and then come back to see your edits.
+           </Section>
+           <Section title="See Your Changes">
+             <ReloadInstructions />
+           </Section>
+           <Section title="Debug">
+             <DebugInstructions />
+           </Section>
+           <Section title="Learn More">
+             Read the docs to discover what to do next:
+           </Section>
+           <LearnMoreLinks />
+         </View>
+       </ScrollView>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   sectionContainer: {
+     marginTop: 32,
+     paddingHorizontal: 24,
+   },
+   sectionTitle: {
+     fontSize: 24,
+     fontWeight: '600',
+   },
+   sectionDescription: {
+     marginTop: 8,
+     fontSize: 18,
+     fontWeight: '400',
+   },
+   highlight: {
+     fontWeight: '700',
+   },
+ });
+ 
+ export default App;
+ 
--- NewRelicExampleApp/index.js
+++ NewRelicExampleApp/index.js
@@ -2,12 +2,19 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-import * as keys from './newrelic.json';
-import NewRelic from 'newrelic-react-native-agent';
-
-NewRelic.startAgent(keys.licenseKey.android);
-NewRelic.recordCustomEvent("MobileModularAgentTest", "react-native-modular-agent", {"platform1": Platform.OS, "appDidMount": "true"});
-AppRegistry.registerComponent(appName, () => App);
+ import {AppRegistry} from 'react-native';
+ import App from './App';
+ import {name as appName} from './app.json';
+ import * as keys from './newrelic.json';
+ import NewRelic from 'newrelic-react-native-agent';
+ 
+ //start the Agent
+ NewRelic.startAgent(keys.licenseKey.ios);
+ //set Attribute
+ NewRelic.setAttribute("NewRelic RN App","1.0.0")
+ // Adding Custom Event
+ NewRelic.recordCustomEvent("MobileModularAgentTest", "react-native-modular-agent", {"platform1": Platform.OS, "appDidMount": "true"});
+ NewRelic.setJSAppVersion("0.1.2");
+ 
+ AppRegistry.registerComponent(appName, () => App);
+ 
