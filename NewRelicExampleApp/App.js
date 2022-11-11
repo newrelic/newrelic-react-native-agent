f4cfcf3893ad58acc68488984e52e39e755c6639
Kenny <kthai@newrelic.com>
2022-11-11 11:13:56 -0800

    feat: New example app

--- NewRelicExampleApp/App.js
+++ NewRelicExampleApp/App.js
@@ -1,204 +1,29 @@
-/*
- * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0 
- */
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
 
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import ApiScreen from './screens/Api';
+import ExamplesScreen from './screens/Examples';
 
- import React from 'react';
- import NewRelic from 'newrelic-react-native-agent';
- import type {Node} from 'react';
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
-   Button
- } from 'react-native';
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
- 
- const Section = ({children, title}): Node => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
- 
- const App: () => Node = () => {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
- 
-   //Http Error and InterAction Example
-   const badApiLoad = async () => {
-     const interactionId = await NewRelic.startInteraction('StartLoadBadApiCall');
-     console.log(interactionId);
-     const url = 'https://facebook.github.io/react-native/moviessssssssss.json';
-     fetch(url)
-       .then((response) => response.json())
-       .then((responseJson) => {
-         console.log(responseJson);
-         NewRelic.endInteraction(interactionId);
-       }) .catch((error) => {
-         NewRelic.endInteraction(interactionId);
-         console.error(error);
-       });;
-   };
- 
-   // Http Request and Interaction Example
-   const goodApiLoad = async () => {
-     const interactionId = await NewRelic.startInteraction('StartLoadGoodApiCall');
-     console.log(interactionId);
-     const url = 'https://facebook.github.io/react-native/movies.json';
- 
-     NewRelic.recordBreadcrumb("Http Request Start",{
-       "url":url,
-       "starttine":new Date()
-     })
-     fetch(url)
-       .then((response) => response.json())
-       .then((responseJson) => {
-         NewRelic.recordBreadcrumb("Http Request Success",{
-           "response":responseJson,
-           "url":url,
-           "endtime":new Date()
-         })
-         console.log(responseJson);
-         NewRelic.endInteraction(interactionId);
-       })
-       .catch((error) => {
-         NewRelic.endInteraction(interactionId);
-         console.error(error);
-       });
-   };
- 
-   //Promise Rejection Example
-   // if user is not handling error in catch then it captures by our prmose rejection handler
-   const promiseRejection = async () => {
-    //  const interactionId = await NewRelic.startInteraction('PromiseRejectionCall');
-     console.log(interactionId);
-     const url = 'https://facebook.github.io/react-native/moviessssssssss.json';
-     fetch(url)
-       .then((response) => response.json())
-       .then((responseJson) => {
-         console.log(responseJson);
-         NewRelic.endInteraction(interactionId);
-       }) 
-   };
- 
-   // JS Error Example
-   // When Javascript throws error it captures by our Error Handler
-   const throwError = () => {
-      NewRelic.removeAttribute("")
-      throw new Error("I am Error");
-   }
- 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <Button title={'Bad API'} onPress={badApiLoad} color={'#3365f3'} />
- 
-       <View style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,height: 10
-           }}></View>
- 
-       <Button title={'Good API'} onPress={goodApiLoad} color={'#3365f3'} />
- 
-       <View style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,height: 10
-           }}></View>
- 
-      <Button title={'Promise Rejection Example'} onPress={promiseRejection} color={'#3365f3'} />
- 
-      <View style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,height: 10
-           }}></View>
- 
-      <Button title={'Error Example'} onPress={throwError} color={'#3365f3'} />
- 
- 
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
- 
- export default App;
- 
+import NewRelic from 'newrelic-react-native-agent';
+
+const Tab = createBottomTabNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer onStateChange={ NewRelic.onStateChange } >
+      <Tab.Navigator>
+        <Tab.Screen 
+          name="APIs"
+          component={ApiScreen}
+        />
+        <Tab.Screen
+          name="Examples"
+          component={ExamplesScreen}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
--- NewRelicExampleApp/index.js
+++ NewRelicExampleApp/index.js
@@ -1,28 +1,57 @@
-/*
- * Copyright (c) 2022-present New Relic Corporation. All rights reserved.
- * SPDX-License-Identifier: Apache-2.0 
- */
-
 /**
  * @format
  */
 
- import {AppRegistry} from 'react-native';
- import App from './App';
- import {name as appName} from './app.json';
- import * as keys from './newrelic.json';
- import NewRelic from 'newrelic-react-native-agent';
- import * as appversion from './package.json';
 
- //start the Agent
- NewRelic.startAgent(keys.licenseKey.ios);
- NewRelic.setJSVersion(appversion.version);
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import NewRelic from 'newrelic-react-native-agent';
+import {name, version} from './package.json';
+import {Platform} from 'react-native';
 
- //set Attribute
- NewRelic.setAttribute("NewRelic RN App","1.0.0")
- // Adding Custom Event
- NewRelic.recordCustomEvent("MobileModularAgentTest", "react-native-modular-agent", {"platform1": Platform.OS, "appDidMount": "true"});
- NewRelic.setJSAppVersion("0.1.2");
- 
- AppRegistry.registerComponent(appName, () => App);
- 
+    let appToken;
+
+    if (Platform.OS === 'ios') {
+        // appToken='';
+        appToken = '*******';
+    } else {
+        // appToken='';
+        appToken = '*******';
+    }
+
+ const agentConfiguration = {
+    
+    //Android Specific
+    // Optional:Enable or disable collection of event data.
+    analyticsEventEnabled: true,
+
+    // Optional:Enable or disable crash reporting.
+    crashReportingEnabled: true,
+
+    // Optional:Enable or disable interaction tracing. Trace instrumentation still occurs, but no traces are harvested. This will disable default and custom interactions.
+    interactionTracingEnabled: true,
+
+    // Optional:Enable or disable reporting successful HTTP requests to the MobileRequest event type.
+    networkRequestEnabled: true,
+
+    // Optional:Enable or disable reporting network and HTTP request errors to the MobileRequestError event type.
+    networkErrorRequestEnabled: true,
+
+    // Optional:Enable or disable capture of HTTP response bodies for HTTP error traces, and MobileRequestError events.
+    httpRequestBodyCaptureEnabled: true,
+
+   //Android Specific
+   // Optional: Enable or disable agent logging.
+    loggingEnabled: true,
+
+    //iOS Specific
+    // Optional:Enable/Disable automatic instrumentation of WebViews
+    webViewInstrumentation: true
+  };
+
+NewRelic.startAgent(appToken,agentConfiguration);
+NewRelic.setJSAppVersion(version);
+AppRegistry.registerComponent(name, () => App);
+
+AppRegistry.registerComponent(appName, () => App);
